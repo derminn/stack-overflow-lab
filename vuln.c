0efd836a437bcb8dd8c232f53c8abf42f129eeff
derminnn <dharminpanchal24@gmail.com>
2026-03-05 22:09:43 +0530

    Update vuln.c

--- vuln.c
+++ vuln.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <string.h>
 
+void secret() {
+    printf("You reached the secret function!\n");
+}
+
+void vuln() {
+    char buffer[32];
+
+    printf("input : ");
+    gets(buffer);   // vulnerable function
+
+    printf("you entered : %s\n", buffer);
+}
+
+int main() {
+    vuln();
+    return 0;
+}
